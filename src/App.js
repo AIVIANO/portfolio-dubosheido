2286f2261729e75f8d40edfe089b762a381bc1f8
AIVIANO <dub.dmirij@gmail.com>
2024-06-12 23:25:29 +0300

    fix text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import cat1 from './images/cat1.jpg';
 import cat2 from './images/cat2.jpg';
@@ -12,15 +12,19 @@ import val1 from './images/val1.jpg';
 import val2 from './images/val2.jpg';
 
 function App() {
+  useEffect(() => {
+    document.title = "Портфолио Дмитрия";
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Портфолио</h1>
         <p>
-          Добрый день, это сайт - портфолио, которое сделано на React, раз это портфолио то я расскажу о себе.
+          Добрый день, это сайт - портфолио, которое сделано на React, раз это портфолио то я расскажу о себе. 
           Меня зовут Дмитрий и я студент Финансового Университета, учусь на 3 курсе по направлению подготовки "прикладная информатика".
         </p>
-        <p>Раньше я увлекался хоккеем и настольным теннисом</p>
+        <p>Раньше я увлекался хоккеем и настольным теннисом.</p>
         <p>А вот моя кошка Дуся:</p>
         <div className="gallery">
           <img src={cat1} alt="Cat 1" />
@@ -32,7 +36,7 @@ function App() {
           <img src={cat7} alt="Cat 7" />
           <img src={cat8} alt="Cat 8" />
         </div>
-        <p>А что касается сегодняшней погоды:</p>
+        <p>А что касается сегодняшней погоды, я убедился что под деревьями лучше машины не ставить:</p>
         <div className="weather-gallery">
           <img src={val1} alt="Weather 1" />
           <img src={val2} alt="Weather 2" />
